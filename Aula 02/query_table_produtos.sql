fa0b9462ead946e88eb696d64b917ebec1d25a53
Ruan Machado <whiterun092@gmail.com>
2025-07-10 00:19:47 -0300

    feat: Class 04

--- Aula 02/query_table_produtos.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE produtos(
-
-	id_produto INT AUTO_INCREMENT PRIMARY KEY,
-    nome_produto VARCHAR(100) NOT NULL,
-    preco_unitario DECIMAL(10, 2) NOT NULL,
-    estoque INT NOT NULL,
-    ativo BOOLEAN DEFAULT 1
-);
